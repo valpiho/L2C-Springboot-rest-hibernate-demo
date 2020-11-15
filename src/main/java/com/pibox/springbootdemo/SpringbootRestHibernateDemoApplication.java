9ec408e752cadbb0be882c697b85fe33129dd167
Valentin Piho <val.piho@gmail.com>
2020-11-15 12:37:51 +0200

    init

---
+++ src/main/java/com/pibox/springbootdemo/SpringbootRestHibernateDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pibox.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestHibernateDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRestHibernateDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pibox/springbootdemo/SpringbootRestHibernateDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pibox.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestHibernateDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
